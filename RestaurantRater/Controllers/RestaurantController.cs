2e50ac98fa744cb6f1d805e7ddcfac88227c79a2
Stephen <>
2019-01-30 22:18:30 -0500

    Initial commit, first attempt at MVC app

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDataBaseContext database = new RestaurantDataBaseContext();
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(database.Restaurants.ToList());
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost][ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                database.Restaurants.Add(restaurant);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+            
+        }
+    }
+}
---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Rating { get; set; }
+
+    }
+        public class RestaurantDataBaseContext : DbContext
+        {
+            public DbSet<Restaurant> Restaurants { get; set; }
+        }
+}
